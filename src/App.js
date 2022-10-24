1e240161d86b9bef22e99a85369fdd1e1827fad2
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2022-10-24 05:15:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Practice from "./pages/Practice";
 import Rank from "./pages/Rank";
 function App() {
