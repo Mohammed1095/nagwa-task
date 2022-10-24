cd21f57c569d9ac06459dab502aad3d86473d614
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2022-10-24 06:20:44 +0200

    adding-MUI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Practice from "./pages/Practice";
 import Rank from "./pages/Rank";
+import { Container } from "@mui/material";
+import { Box } from "@mui/system";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" exact>
-          <Practice />
-        </Route>
-        <Route path="/rank">
-          <Rank />
-        </Route>
-      </Routes>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Routes>
+            <Route path="/" element={<Practice />} />
+            <Route path="/practice" element={<Practice />}></Route>
+            <Route path="/rank" element={<Rank />}></Route>
+          </Routes>
+        </Box>
+      </Container>
     </Router>
   );
 }
